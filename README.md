189a97a5ff67789df40aed17c781df8d97ed6d1d
tmatijev <matas0412@gmail.com>
2017-03-04 10:34:44 +0100

    Add files
    - Add .gitignore
    - Add .npmignore
    - Modify README.md
    - Add matchResultPrediction code
    - Add package.json

---
+++ README.md
@@ -0,0 +1,1 @@
+# Match Result Prediction
---
+++ matchResultPrediction.js
@@ -0,0 +1,33 @@
+module.exports = function (d) {
+  /**
+   * d.homeTeamCoef {Number}
+   * d.awayTeamCoef {Number}
+   * d.drawMax {Number}
+   * d.winMax {Number}
+   */
+
+  let random = Math.random()
+  let randomDraw = Math.floor(Math.random() * d.drawMax)
+  let randomWin = Math.floor(Math.random() * d.winMax) + 1
+  let biggerCoef = d.homeTeamCoef > d.awayTeamCoef ? d.homeTeamCoef : d.awayTeamCoef
+  let smallerCoef = d.homeTeamCoef > d.awayTeamCoef ? d.awayTeamCoef : d.homeTeamCoef
+  let drawChance = (1 - (biggerCoef - smallerCoef)) / 3
+  let homeTeamScore = 0
+  let awayTeamScore = 0
+
+  if (random < drawChance) {
+    homeTeamScore = randomDraw
+    awayTeamScore = randomDraw
+  } else if (random < d.homeTeamCoef) {
+    homeTeamScore = randomWin;
+    awayTeamScore = randomWin === 1 ? 0 : Math.floor(Math.random() * randomWin - 1) + 1
+  } else {
+    awayTeamScore = randomWin
+    homeTeamScore = randomWin === 1 ? 0 : Math.floor(Math.random() * randomWin - 1) + 1
+  }
+
+  return {
+    homeTeamScore,
+    awayTeamScore
+  }
+}
